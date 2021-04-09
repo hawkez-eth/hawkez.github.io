68cf19aa55fd90d320567ae6229873b82151afc9
importAnt <ken14250@gmail.com>
2021-04-09 16:49:42 +0800

    updated project info

--- src/components/ProjectGallery.js
+++ src/components/ProjectGallery.js
@@ -13,27 +13,35 @@ import React from "react"
 import { Fade, Slide } from "react-reveal"
 
 const projects = [
-  {
+  /*{
     title: "BunTouEShake",
     desc:
       "Full list of information about shops, restaurants and YouTubers. All consolidated on one platform",
     link: "https://buntoueshake.com/",
     src: "https://antyslash.com/wp-content/uploads/2020/04/wongYip-360x240.jpg",
     tags: "Web",
+  },*/
+  {
+    title: "TWGHs Zonta Club of Kowloon Nursery School",
+    desc:
+      "As a developer, my job is to make sure the requirements about the design and copywriting provided by clients are fully addressed, and have this website presented in a way that fulfills RWD but also looks lovely and energetic at the same time.",
+    link: "https://zckns.tungwahcsd.org/tc/page/home-page",
+    src: "../../60601c9f5c6a4.png",
+    tags: "Web",
   },
   {
     title: "PolyU Photo Competition",
     desc:
-      "A project that I had taken the lead on which is about a photo competion campaign, I worked closely with The Hong Kong Polytechnic University to make sure the layout is neat and clear.",
+      "A project that I had taken the lead on which is about a photo competion campaign hosted by The Hong Kong Polytechnic University, I worked closely with the campaign team of the competition of Poly U to make sure the layout is clear and refreshing.",
     link: "https://www.polyu.edu.hk/cpa/polyucampus/index.html/",
     src:
-      "https://www.polyu.edu.hk/cpa/polyucampus/images/10yammi_seize-21d.jpg",
+      "https://antyslash.com/wp-content/uploads/2019/03/10yammi_seize-21d-360x240.jpg",
     tags: "Web",
   },
   {
     title: "SOUL Offical Website",
     desc:
-      "As a web developer of SOUL®️, my job is to keep the website running smooth and error-free in order to give our customers the best shopping experience. Mainly I use HTML, CSS, Javascript and Liquid as the programming languages for this project.",
+      "While ensuring the reputation and credibility of SOUL®️ during the revamp of the brand, my work is to also keep the website running smooth and error-free in order to give our customers the best ecommerce shopping experience.",
     link: "https://soulnation.com/",
     src: "https://i.ytimg.com/vi/W4jNyiWBHlA/maxresdefault.jpg",
     tags: "Web",
@@ -74,8 +82,24 @@ const ProjectGallery = () => {
                           "0 1px 2px rgb(0 0 0 / 5%), 0 15px 30px rgb(0 0 0 / 10%)",
                       }}
                       src={project.src}
-                      alt={`Project ${index}`}
+                      alt={project.title}
+                      height="180px"
+                      mx="auto"
                     />
+                    {/*<Box
+                      width="100%"
+                      boxShadow="0 1px 2px rgb(0 0 0 / 5%), 0 10px 20px rgb(0 0 0 / 5%)"
+                      transition=".8s cubic-bezier(.2,.8,.2,1)"
+                      _groupHover={{
+                        transform: "translateY(-5px)",
+                        boxShadow:
+                          "0 1px 2px rgb(0 0 0 / 5%), 0 15px 30px rgb(0 0 0 / 10%)",
+                      }}
+                      bgImage={`url(${project.src})`}
+                      bgPosition="center"
+                      bgRepeat="no-repeat"
+                      height="250px"
+                    ></Box>*/}
                   </LinkOverlay>
                   <Box my={4}>
                     <Tag>{project.tags}</Tag>
